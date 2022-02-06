3c1e7716e855fb6a0cf297ace03144a9fc7b9a62
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-02-06 14:19:42 +0000

    Add className property to Container

--- src/lib/elements/base/Container.tsx
+++ src/lib/elements/base/Container.tsx
@@ -1,12 +1,18 @@
 import React from "react"
 
 interface ContainerProps {
+    className?: string
     children?: React.ReactNode
 }
 
-export function Container({ children }: ContainerProps) {
+export function Container({ className = "", children }: ContainerProps) {
     return (
-        <div className="rounded-md border-2 border-neutral-100 bg-neutral-50 p-6 dark:border-neutral-700 dark:bg-neutral-800">
+        <div
+            className={
+                "rounded-md border-2 border-neutral-100 bg-neutral-50 p-6 dark:border-neutral-700 dark:bg-neutral-800" +
+                ` ${className}`
+            }
+        >
             {children}
         </div>
     )
