0b694cc48b293d50ba945516a85542e1046989b1
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-02-06 14:20:35 +0000

    Adjust dark hover colour for paragraphLink

--- src/lib/typography/index.ts
+++ src/lib/typography/index.ts
@@ -4,7 +4,7 @@ const typography = {
     largeTitle: "text-3xl font-bold dark:text-neutral-300",
     paragraph: "text-lg text-neutral-700 dark:text-neutral-400",
     paragraphLink:
-        "font-semibold dark:bg-emerald-400/10 hover:text-emerald-800 hover:bg-emerald-500/50 bg-emerald-400/40 text-emerald-800/60 dark:text-emerald-400/80 dark:hover:text-emerald-900 dark:hover:bg-emerald-400/60 py-1 px-2 rounded-md transition-all"
+        "font-semibold dark:bg-emerald-400/10 hover:text-emerald-800 hover:bg-emerald-500/50 bg-emerald-400/40 text-emerald-800/60 dark:text-emerald-400/80 dark:hover:text-emerald-900 dark:hover:bg-emerald-400/70 py-1 px-2 rounded-md transition-all"
 }
 
 export default typography
