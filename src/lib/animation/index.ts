26dc39115a3cc7191ae0e26989a4bc74d0870ca1
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-02-06 13:28:55 +0000

    Setup animations

---
+++ src/lib/animation/index.ts
@@ -0,0 +1,9 @@
+const variants = {
+    fadeInOut: {
+        initial: { opacity: 0 },
+        animate: { opacity: 1 },
+        exit: { opacity: 0 }
+    }
+}
+
+export { variants }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import { AnimatePresence, motion } from "framer-motion"
+import { variants } from "../lib/animation"
+import { useRouter } from "next/router"
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    const router = useRouter()
+
+    return (
+        <AnimatePresence>
+            <motion.div
+                key={router.asPath}
+                variants={variants.fadeInOut}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                transition={{ type: "linear", duration: 0.35 }}
+            >
+                <Component {...pageProps} />
+            </motion.div>
+        </AnimatePresence>
+    )
 }
