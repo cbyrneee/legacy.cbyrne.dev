8ef9859587043de870d88e29764f6fd30bfc7126
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-02-06 17:19:28 +0000

    Remove framer-motion

--- src/lib/animation/index.ts
+++
@@ -1,9 +0,0 @@
-const variants = {
-    fadeInOut: {
-        initial: { opacity: 0 },
-        animate: { opacity: 1 },
-        exit: { opacity: 0 }
-    }
-}
-
-export { variants }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,10 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
-import { AnimatePresence, motion } from "framer-motion"
-import { variants } from "../lib/animation"
-import { useRouter } from "next/router"
 
 export default function App({ Component, pageProps }: AppProps) {
-    const router = useRouter()
-
     return (
-        <AnimatePresence>
-            <motion.div
-                className="container mx-auto w-full max-w-4xl p-4 lg:p-0 lg:py-12"
-                key={router.asPath}
-                variants={variants.fadeInOut}
-                initial="initial"
-                animate="animate"
-                exit="exit"
-                transition={{ type: "linear", duration: 0.35 }}
-            >
-                <Component {...pageProps} />
-            </motion.div>
-        </AnimatePresence>
+        <div className="container mx-auto w-full max-w-4xl p-4 lg:p-0 lg:py-12">
+            <Component {...pageProps} />
+        </div>
     )
 }
