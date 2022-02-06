7677aa4ab0db9571e2af349ebab00754a72192b0
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-02-06 14:19:53 +0000

    Fix text spacing on mobile

--- src/lib/elements/home/AboutMe.tsx
+++ src/lib/elements/home/AboutMe.tsx
@@ -6,15 +6,17 @@ export function AboutMe() {
     return (
         <Container>
             <h3 className={typography.normalTitle}>About Me</h3>
-            <p className={typography.paragraph}>
-                I am a student and developer, with proficiency in Kotlin, Java, Typescript and Swift.
-            </p>
-            <p className={typography.paragraph}>
-                Currently, I am working for{" "}
-                <a className={typography.paragraphLink} href="https://oplegends.com">
-                    OPLegends.
-                </a>
-            </p>
+            <div className="flex flex-col gap-2 lg:gap-0">
+                <p className={typography.paragraph}>
+                    I am a student and developer, with proficiency in Kotlin, Java, Typescript and Swift.
+                </p>
+                <p className={typography.paragraph}>
+                    Currently, I am working for{" "}
+                    <a className={typography.paragraphLink} href="https://oplegends.com">
+                        OPLegends.
+                    </a>
+                </p>
+            </div>
         </Container>
     )
 }
