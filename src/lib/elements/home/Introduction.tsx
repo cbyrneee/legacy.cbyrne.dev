becb6082dd66e9e0615b8407a5fd924792fc3a6e
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-02-06 16:41:56 +0000

    Layout fixes on the Introduction element

--- src/lib/elements/home/Introduction.tsx
+++ src/lib/elements/home/Introduction.tsx
@@ -8,7 +8,7 @@ import { ChatIcon, CodeIcon, EmailIcon } from "../../components/icons"
 export function Introduction() {
     return (
         <Container>
-            <div className="w-full items-center gap-4 lg:flex lg:flex-row lg:justify-between">
+            <div className="w-full items-center gap-4 px-4 lg:flex lg:flex-row lg:justify-between">
                 <div className="flex flex-row items-center gap-4">
                     <div className="h-20 w-20">
                         <Image className="rounded-md" alt="Profile" width="100px" height="100px" src="/profile.webp" />
@@ -20,15 +20,15 @@ export function Introduction() {
                     </div>
                 </div>
 
-                <div className="mt-4 flex flex-row gap-2">
-                    <Link href="https://github.com/cbyrneee" bigger>
-                        <div className="flex flex-row gap-2">
-                            <CodeIcon className="hidden h-6 w-6 md:inline" /> GitHub
-                        </div>
-                    </Link>
+                <div className="mt-4 flex flex-row gap-2 lg:mt-0">
                     <Link href="mailto:hello@cbyrne.dev" bigger>
                         <div className="flex flex-row gap-2">
                             <EmailIcon className="hidden h-6 w-6 md:inline" /> Email
+                        </div>
+                    </Link>
+                    <Link href="https://github.com/cbyrneee" bigger>
+                        <div className="flex flex-row gap-2">
+                            <CodeIcon className="hidden h-6 w-6 md:inline" /> GitHub
                         </div>
                     </Link>
                     <Link href="https://twitter.com/cbyrnedev" bigger>
